6dc093d37d28cb5ba92d2e3aa65c2547e223791b
chirag prajapati <cp920848@gmail.com>
2026-06-23 13:17:40 +0530

    Final Update

--- backend/server.js
+++ backend/server.js
@@ -41,11 +41,14 @@ mongoose
   });
 
 const transporter = nodemailer.createTransport({
-  service: "gmail",
+  host: "smtp.gmail.com",
+  port: 587,
+  secure: false,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
   },
+  family: 4,
 });
 
 transporter.verify((err, success) => {
